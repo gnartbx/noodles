b38f2eeb9bd6249821a952e290c412f8ca97ca49
gnartbx <gnartbx@users.noreply.github.com>
2017-09-09 15:27:43 +0000

    Add a command-line flag for board size.

--- nq/nq.cc
+++ nq/nq.cc
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <gflags/gflags.h>
 
 #include "queens.h"
 
+DEFINE_int32(board_size, 8,
+             "Number of rows/columns in the chess boards.");
+
+
 int main(int argc, char** argv) {
-  nq::Queens q = nq::Queens::Create(8);
-  std::cout << "num_attacks(8) = " << q.num_attacks() << std::endl;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  nq::Queens q = nq::Queens::Create(FLAGS_board_size);
   std::cout << "Board state: " << q << std::endl;
   return 0;
 }
